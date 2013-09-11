0591d02eb77bb1a543a0112b9afe6588f153a57d
Lambert89 <jason.lambert.89@gmail.com>
2013-09-11 18:43:17 +0100

    implementing the usermgmt service.

---
+++ com-czelink-usermgmt-server/src/main/java/com/czelink/usermgmt/controllers/UsermgmtController.java
@@ -0,0 +1,8 @@
+package com.czelink.usermgmt.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UsermgmtController {
+	// TODO: to implement.
+}
---
+++ com-czelink-usermgmt-server/src/main/java/com/czelink/usermgmt/handlers/AjaxAuthenticationSuccessHandler.java
@@ -0,0 +1,36 @@
+package com.czelink.usermgmt.handlers;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONObject;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import com.czelink.usermgmt.constants.UsermgmtConstants;
+
+public class AjaxAuthenticationSuccessHandler implements
+		AuthenticationSuccessHandler {
+
+	public void onAuthenticationSuccess(HttpServletRequest request,
+			HttpServletResponse response, Authentication auth)
+			throws IOException, ServletException {
+
+		final String token = UUID.randomUUID().toString();
+
+		final JSONObject result = new JSONObject();
+		result.put("status", true);
+		result.put("token", token);
+
+		request.getSession().setAttribute(UsermgmtConstants.LOGON_TOKEN_KEY,
+				token);
+
+		response.getWriter().print(result.toString());
+		response.getWriter().flush();
+	}
+}
