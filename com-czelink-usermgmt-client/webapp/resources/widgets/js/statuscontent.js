7d4bd49c5b6a704681e96d65c220761e2fcab252
Lambert89 <jason.lambert.89@gmail.com>
2013-09-22 15:37:20 +0100

    implemented registration user interface.

--- com-czelink-usermgmt-client/webapp/resources/widgets/js/statuscontent.js
+++ com-czelink-usermgmt-client/webapp/resources/widgets/js/statuscontent.js
@@ -46,13 +46,21 @@ define(function() {
 			$(loginModal).modal('show');
 		});
 
+		$scope.disableDuringSubmit = false;
+
 		$scope.initLoginInfoPanelInvalid = false;
 
 		$scope.checkIfLoginInfoInvalid = function() {
 			return $scope.initLoginInfoPanelInvalid
 					&& ($scope.loginForm.username.$error.required
+							|| $scope.loginForm.password.$error.maxlength
+							|| $scope.loginForm.password.$error.minlength
 							|| $scope.loginForm.username.$error.email
 							|| $scope.loginForm.password.$error.required || $scope.loginfailedMessage);
+		};
+
+		$scope.checkLoginButtonStatus = function() {
+			return ($scope.isRegisterPanelActivated() || $scope.disableDuringSubmit);
 		};
 
 		var resetLoginModel = function() {
@@ -62,6 +70,9 @@ define(function() {
 		};
 
 		$scope.login = function() {
+
+			$scope.disableDuringSubmit = true;
+
 			$scope.initLoginInfoPanelInvalid = true;
 			$scope.loginfailedMessage = false;
 
@@ -83,6 +94,8 @@ define(function() {
 						$scope.loginfailedMessage = true;
 					}
 
+					$scope.disableDuringSubmit = false;
+
 					if (!$scope.$$phase) {
 						$scope.$apply();
 					}
@@ -92,6 +105,8 @@ define(function() {
 				secureDataRetriever.onFailure(function() {
 					$scope.loginfailedMessage = true;
 
+					$scope.disableDuringSubmit = false;
+
 					if (!$scope.$$phase) {
 						$scope.$apply();
 					}
@@ -99,6 +114,8 @@ define(function() {
 				});
 
 				secureDataRetriever.post("usermgmt/login");
+			} else {
+				$scope.disableDuringSubmit = false;
 			}
 		};
 
@@ -106,8 +123,90 @@ define(function() {
 			return $scope.loginfailedMessage;
 		};
 
+		$scope.checkPreRegisterStatusValid = function() {
+			return ($scope.newpassword === $scope.confirmpassword);
+		};
+
+		$scope.checkPostRegisterStatusInvalid = function() {
+			return ($scope.registerResult === 0);
+		};
+
+		$scope.registerInvalidInit = false;
+
+		$scope.registerResult = -1;
+		$scope.registerFailReason = "";
+
+		$scope.checkIfRegisterInfoInvalid = function() {
+			return $scope.registerInvalidInit
+					&& ($scope.registerForm.newusername.$error.required
+							|| $scope.registerForm.newusername.$error.email
+							|| $scope.registerForm.newpassword.$error.required
+							|| $scope.registerForm.newpassword.$error.maxlength
+							|| $scope.registerForm.newpassword.$error.minlength
+							|| $scope.registerForm.confirmpassword.$error.maxlength
+							|| $scope.registerForm.confirmpassword.$error.minlength
+							|| !$scope.checkPreRegisterStatusValid() || ($scope.registerResult === 0));
+		};
+
+		$scope.checkRegisterStatus = function() {
+			if ($scope.registerResult === 1) {
+				return true;
+			} else {
+				return false;
+			}
+		};
+
 		$scope.register = function() {
-			// TODO: to finish.
+
+			$scope.disableDuringSubmit = true;
+
+			$scope.registerInvalidInit = true;
+			$scope.registerResult = -1;
+
+			if (!$scope.checkIfRegisterInfoInvalid()) {
+				$scope.registerInvalidInit = true;
+
+				secureDataRetriever.setData({
+					username : $scope.newusername,
+					password : $scope.newpassword
+				});
+
+				secureDataRetriever.onFailure(function() {
+					$scope.disableDuringSubmit = false;
+					return false;
+				});
+
+				secureDataRetriever.onSuccess(function(data) {
+
+					if (data.status) {
+						$scope.registerResult = 1;
+					} else {
+						$scope.registerResult = 0;
+						if (data.statusCode === "002") {
+							$scope.registerFailReason = "该邮箱已经注册";
+						}
+						if (data.statusCode == "008") {
+							$scope.registerFailReason = "服务器异常，请联系管理员";
+						}
+					}
+
+					$scope.disableDuringSubmit = false;
+
+					if (!$scope.$$phase) {
+						$scope.$apply();
+					}
+					return false;
+				});
+
+				secureDataRetriever.post("usermgmt/register");
+			} else {
+				$scope.disableDuringSubmit = false;
+			}
+		};
+
+		$scope.checkRegisterButtonStatus = function() {
+			return (($scope.isLoginPanelActivated())
+					|| ($scope.registerResult == 1) || $scope.disableDuringSubmit);
 		};
 	};
 });
